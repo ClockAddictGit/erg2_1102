4a6bd548f18e45ab5efd053ad084c32b66517f67
Evelyn Fragia <evelyne.fr@hotmail.com>
2023-11-19 16:41:21 +0200

    c6 - Added average rainfall function

--- rainfall.f90
+++ rainfall.f90
@@ -2,36 +2,48 @@ program Rainfall
     
     implicit none
     
-    integer:: i, n
-    character(15),dimension(50):: town
-    real,dimension(50):: rain
     character(1)::ans
+    character(15),dimension(50):: town
+    integer:: i, n
+    real:: mean, m
+    real,dimension(50):: rain
+    
     
     call read_print_data (n, town, rain)
+    mean=mean_rainfall(rain,n)
         
-!   Printing data from data.txt ---------------------------------------------------------------------------------------    
+!   Printing data from data.txt ----------------------------------------------------------------------------------
     write(*,100)
 100 FORMAT ('Town',16x,'Rain') 
-    do i=1,n-1
+    do i=1,n
         write(*,200) town(i), rain(i)
     end do
 200 FORMAT (A15,5x,F6.1)
     
-!Exit from console ---------------------------------------------------------------------------------------
+    write(*,'    ')
+    
+!   Printing mean value of rainfall
+    write(*,300) mean
+300 FORMAT ('The mean value of rainfall of all cities is',1x,F6.1)
+    
+    write(*,'    ')
+    
+!   Exit from console --------------------------------------------------------------------------------------------
     write (*,*) 'Press y to exit'
-300 read (*,*) ans    
+400 read (*,*) ans    
     if (ans=='y') then
         stop
     else 
-        go to 300
+        go to 400
     end if
     
+!------------------------------------------------------------------------------------------------------------------   
+!------------------------------------------------------------------------------------------------------------------   
     
-   
 !   Diadikasies 
     contains
     
-!   Yporoutina diabasmatos kai apeikonisis data.txt
+!   Yporoutina diabasmatos kai apeikonisis data.txt----------------------------------------------------------------
     subroutine read_print_data (nmax,T,R)
     
         integer:: k, nmax, st
@@ -46,12 +58,28 @@ program Rainfall
             nmax=0
             do k=1,50
                 read(1,*,iostat=st) T(k), R(k)
-                nmax=nmax+1
                 if (st==-1) exit
+                nmax=nmax+1
             end do
         end if
     
     end subroutine read_print_data
+    
+!   Sunartisi upologismou mesou orou vroxoptwsis------------------------------------------------------------------
+    function mean_rainfall(x,p) result(m)
+        
+        integer:: p, k
+        real:: m
+        real,dimension(p):: x, y
+        
+        m=0
+        do k=1,p
+            m=x(k)+m
+        end do
+        m=m/p
+           
+    end function
+    
     
     
 end program Rainfall
